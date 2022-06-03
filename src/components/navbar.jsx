6c49e1ff6cc9754118a6ed3d45d4833d7ee4cb56
Ashvin <ashvinsunga@yahoo.com.ph>
2022-06-03 15:09:33 +0800

    event raiser, handlers and lifted state done

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav class='navbar bg-light'>
+        <div class='container-fluid'>
+          <a class='navbar-brand' href='#'>
+            Navbar
+            <span>{this.props.totalCounters}</span>
+          </a>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
